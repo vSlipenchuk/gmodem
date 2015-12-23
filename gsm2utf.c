19fbe04fe78b23cd99ec7670125ef1ff652d1668
Vladimir Slipenchuk <vslipenchuk@gmail.com>
2015-12-23 12:51:51 +0300

    sms list gsm2utf corrected

--- gsm2utf.c
+++ gsm2utf.c
@@ -1,11 +1,14 @@
 //gsm2utf(sms->text,sms->ud,sms->udl);
 
 int gsm2utf(char *out,char *ucs2,int len) { // converts UCS2 -> utf8, len in bytes
+//hexdump("gsm2utf:",ucs2,len);
 int tlen=0;
     while(len>0) {
-      int wch  = ( ucs2[1]<<8 | ucs2[0] ); // first go page
-      int rlen = utf8_poke(out,wch,1);
-    len-=2; ucs2+=2;  tlen+=rlen;
+      int wch  = ( ucs2[0]<<8 | ucs2[1] ); // first go page
+      int rlen = utf8_poke(out,wch,4);
+      //printf("rlen=%d wch=%d\n",rlen,wch);
+      //hexdump("U",out,rlen); hexdump("rest",ucs2,len);
+      len-=2; ucs2+=2;  tlen+=rlen;
     if (out) { out+=rlen;   }
     }
 if (out) *out=0;
